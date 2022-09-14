f9dd5a678926882d08d0dbc6d0ccc6e5cd268a67
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-14 13:10:10 +0300

    Factorial works

---
+++ MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example5
+{
+    public static class MyMath
+    {
+        public static int Factorial(int n)
+        {
+            int result = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Example5
     {
         static void Main(string[] args)
         {
+            int a;
+            int answer;
+            while (1 == 1)
+            {
+                Console.WriteLine("Give me a number");
+                a = int.Parse(Console.ReadLine());
+                answer = MyMath.Factorial(a);
+                Console.WriteLine("The factorial of " + a + " is " + answer);
+            }
+
         }
     }
 }
